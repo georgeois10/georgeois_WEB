848cfeb2e67367f4d004976729e9017ac29db9cd
lv2jin <130518725+lv2jin@users.noreply.github.com>
2023-10-31 15:22:47 +0900

    feature/vuesetting
    vue setting

---
+++ Georgeois/src/main/java/com/web/georgeois/GeorgeoisApplication.java
@@ -0,0 +1,13 @@
+package com.web.georgeois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeorgeoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeorgeoisApplication.class, args);
+	}
+
+}
---
+++ Georgeois/src/test/java/com/web/georgeois/GeorgeoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.georgeois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeorgeoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
